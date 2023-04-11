a79a3877831f54005a9987688604e656d1e59d98
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-04-11 15:30:21 -0400

    completed

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ function isPalindrome(word) {
 /* 
   Add your pseudocode here
 */
+function isPalindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+module.exports = isPalindrome;
 
 /*
   Add written explanation of your solution here
